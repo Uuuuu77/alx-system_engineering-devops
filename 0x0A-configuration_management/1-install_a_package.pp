0c61d44531ad38c08f7b1daf9906e7ed832f2cbb
John Njuguna <johnmnjuguna89@gmail.com>
2023-11-25 01:34:07 -0800

    Install a flask package from pip3 using Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/pup
 # Install a flask package from pip3.
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug'],
+}
+
+package { 'werkzeug':
+  ensure    => '2.1.1',
+  provider  => 'pip3',
 }
