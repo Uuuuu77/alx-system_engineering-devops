cc0f004fd4abf6bd87a41dfdfd24ed352f316a4a
John Njuguna <johnmnjuguna89@gmail.com>
2024-02-13 08:46:01 -0800

    Queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+    Query the Reddit API and returns number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Query API to return number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'John'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
