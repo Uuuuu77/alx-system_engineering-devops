b6074c9467495d2ad6de08abd840ad94ae05fc60
John Njuguna <johnmnjuguna89@gmail.com>
2024-02-16 08:23:29 -0800

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Check out why Apache is returning a 500 error using strace.
+
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+  ensure => 'file',
+  source => '/var/www/html/wp-includes/class-wp-locale.php'
+}
